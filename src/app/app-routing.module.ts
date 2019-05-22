2d813871ef94015bf0210b4c9d75676b6ef12caf
Sneka-s <50391659+Sneka-s@users.noreply.github.com>
2019-05-22 12:06:57 +0530

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
+import { DetailsComponent } from './details/details.component';
+import { AccountComponent } from './account/account.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomerComponent } from './customer/customer.component';
+
+
+const routes: Routes = [
+  {
+    path: 'customer',
+    component: CustomerComponent,
+  },
+
+ { path: 'account',
+  component: AccountComponent,
+
+},
+
+{ path: 'details',
+component: DetailsComponent,
+
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/Forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { AccountComponent } from './account/account.component';
+import { DetailsComponent } from './details/details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    AccountComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
